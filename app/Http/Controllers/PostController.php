509b86c2efffae9ebc5ca73e2734d099caad398b
Atsushi <>
2018-08-19 22:22:11 +0800

    modelからデータ取得

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
+
+    private $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
     public function index()
     {
+        $posts = $this->post->get_all_posts();
+        dd($posts);
+        
         return view('post.index');
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function get_all_posts()
+    {
+        return 123;
+    }
 }
